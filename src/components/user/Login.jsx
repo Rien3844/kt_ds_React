fd675c9ddd01d836bbfda46264bd36dfb6e14511
Rien3844 <kuda3844@gmail.com>
2026-05-06 13:03:13 +0900

    login시 새로고침으로 logout되던 현상 제거 => sessionStorage사용

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -1,9 +1,9 @@
 /** @format */
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { isString } from "../../utils/type";
 import { getValidationResult } from "../../utils/errorHandler";
-import { fetchLogin } from "../../http/articles/fetchLogin";
+import { fetchLogin, fetchMyInfo } from "../../http/articles/fetchLogin";
 import { useDispatch, useSelector } from "react-redux";
 import { userAction } from "../../stores/toolkit/slices/userSlice";
 
@@ -11,12 +11,45 @@ const Login = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
 
-  const { token } = useSelector((store) => store.user);
+  const { token, info } = useSelector((store) => store.user);
+
   const toolkitProvider = useDispatch();
+
   const [loginErrors, setLoginErrors] = useState();
 
+  useEffect(() => {
+    toolkitProvider(userAction.autoLogin());
+    const loadMyInfo = async () => {
+      // /api/member/me 호출
+
+      const sessionToken = sessionStorage.getItem("token");
+      const myInfo = await fetchMyInfo(sessionToken);
+
+      if (myInfo.error) {
+        console.log(myInfo);
+        console.log(myInfo.error);
+        // alert(myInfo.error);
+      } else {
+        toolkitProvider(userAction.loadMyInfo(myInfo));
+      }
+    };
+    loadMyInfo();
+  }, [token]);
+
+  // token이 있을 때만 수행.
   if (token) {
-    return <></>;
+    const onLogoutButtonClickHandler = () => {
+      // token이 변조되었거나 만료기간이 도래한 경우.
+      sessionStorage.removeItem("token");
+      // slice store도 제거.
+      toolkitProvider(userAction.logout());
+    };
+    return (
+      <div>
+        {info?.name} ({info?.email})
+        <button onClick={onLogoutButtonClickHandler}>Logout</button>
+      </div>
+    );
   }
 
   const onLoginButtonClickHandler = async () => {
@@ -24,7 +57,11 @@ const Login = () => {
       emailRef.current.value,
       passwordRef.current.value,
     );
-    toolkitProvider(userAction.login(loginResult.token));
+    if (!loginResult.error) {
+      // 로그인 상태를 기억하기 위해서 브라우저의 storage에 등록.
+      sessionStorage.setItem("token", loginResult.token);
+      toolkitProvider(userAction.login(loginResult.token));
+    }
 
     if (loginResult.error) {
       if (isString(loginResult.error)) {
--- src/stores/toolkit/slices/userSlice.js
+++ src/stores/toolkit/slices/userSlice.js
@@ -5,8 +5,23 @@ export const userSlice = createSlice({
   name: "user-slice",
   initialState: {
     token: null,
+    info: null,
   },
   reducers: {
+    logout(store) {
+      store.token = null;
+      store.info = null;
+    },
+    loadMyInfo(store, action) {
+      store.info = action.payload;
+    },
+    autoLogin(store) {
+      // session Storage에 있는 token을 가져와서 userSlice에 등록한다.
+      const token = sessionStorage.getItem("token");
+      if (token) {
+        store.token = token;
+      }
+    },
     login(store, action) {
       store.token = action.payload;
     },
